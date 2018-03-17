eaa2136a9d815b2b955c12500c2bc786971f43c5
Charlie Biddiscombe <33091527+cnopt@users.noreply.github.com>
2018-03-17 23:04:52 +0000

    upload
    - added material icons beside sidenav items

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
   <meta name="keywords" content="keywords,here">
   <link rel="shortcut icon" href="favicon.ico" type="image/vnd.microsoft.icon">
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="script.js" type="text/javascript"></script>
   <link rel="stylesheet" href="style.css" type="text/css">
@@ -18,12 +19,12 @@
     <header>
           <div id="sideNav">
             <a href="javascript:void(0)" class="closebtn" onclick="hideNav()">&times;</a>
-            <a href="#">Politics</a>
-            <a href="#">Society</a>
-            <a href="#">Technology</a>
-            <a href="#">Fashion</a>
-            <a href="#">Entertainment</a>
-            <a href="#">Sport</a>
+            <a href="#"><i class="material-icons">&#xE84F;</i> Politics</a>
+            <a href="#"><i class="material-icons">&#xE84E;</i> Society</a>
+            <a href="#"><i class="material-icons">&#xE1E0;</i> Technology</a>
+            <a href="#"><i class="material-icons">&#xE8F8;</i> Fashion</a>
+            <a href="#"><i class="material-icons">&#xE8DA;</i> Entertainment</a>
+            <a href="#"><i class="material-icons">&#xEB45;</i> Sport</a>
           </div>
 
           <svg onclick="showNav()" width="24px" height="24px" viewBox="0 0 48 48" style="padding-left:1.5rem"><path d="M6 36h36v-4H6v4zm0-10h36v-4H6v4zm0-14v4h36v-4H6z"></path></svg>
